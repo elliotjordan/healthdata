cc48350ec3735671a926829fd084bda2b93bc349
Elliot Jordan <elliotjordan@alumni.iu.edu>
2023-02-06 19:01:29 -0500

    Add script and styles files

---
+++ script.js
@@ -0,0 +1,20 @@
+
+fetch("https://healthdata.gov/resource/7ctx-gtb7.json?$select=state,collection_date,total_staffed_adult_icu_beds&state=KY")
+.then(function(response){
+	return response.json();
+})
+.then(function(fields){
+	let placeholder = document.querySelector("#data-output");
+	let out = "";
+	for(let field of fields){
+		out += `
+			<tr>
+				<td style="">${field.state}</td>
+				<td style="">${field.collection_date}</td>
+				<td style="">${field.total_staffed_adult_icu_beds}</td>
+			</tr>
+		`;
+	}
+
+	placeholder.innerHTML = out;
+});
